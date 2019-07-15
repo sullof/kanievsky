c732dd21b9f5cd0c9c8654b675bb40b94019c8f1
Francesco Sullo <sullof@gmail.com>
2019-07-14 18:24:26 -0700

    Update domains

--- bin/node.sh
+++ bin/node.sh
@@ -9,8 +9,8 @@ source .env && docker run -d \
   --name kanievsky \
   -p 8080 \
   -v $PWD:/usr/src/app \
-  -e VIRTUAL_HOST=kanievsky.com,www.kanievsky.com \
-  -e LETSENCRYPT_HOST=kanievsky.com,www.kanievsky.com \
+  -e VIRTUAL_HOST=kanievsky.com,www.kanievsky.com,gaelkanievsky.com,www.gaelkanievsky.com \
+  -e LETSENCRYPT_HOST=kanievsky.com,www.kanievsky.com,gaelkanievsky.com,www.gaelkanievsky.com \
   -e LETSENCRYPT_EMAIL=admin@kanievsky.com \
   -w /usr/src/app node:carbon npm run start
 
