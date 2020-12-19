4d251cb87aa55f0aaf687a95ab7c3b82e2d56d97
Francesco Sullo <sullof@gmail.com>
2020-12-19 13:36:52 -0800

    Adding fix-sharp

--- bin/node.sh
+++ bin/node.sh
@@ -5,13 +5,11 @@ docker rm kanievsky
 
 #!/usr/bin/env bash
 
-source .env && docker run -d \
+docker run -d \
   --name kanievsky \
   -p 1965 \
   -v $PWD:/usr/src/app \
   -e VIRTUAL_HOST=kanievsky.com,www.kanievsky.com,gaelkanievsky.com,www.gaelkanievsky.com \
   -e LETSENCRYPT_HOST=kanievsky.com,www.kanievsky.com,gaelkanievsky.com,www.gaelkanievsky.com \
-  -e SECRET_SAULT=$SECRET_SAULT \
   -e LETSENCRYPT_EMAIL=admin@kanievsky.com \
   -w /usr/src/app node:12.20.0-alpine3.10 npm run start
-
