26135e6185638e15c73b5d56de4d218a2ea75791
Francesco Sullo <sullof@gmail.com>
2020-12-19 14:50:06 -0800

    Bypass limit?

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 app.use(cors())
 app.use(cookieParser())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 
 app.use('/api/v1', apiV1)
 
